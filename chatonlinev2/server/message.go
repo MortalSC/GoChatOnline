b227bfda58895bcadd86c43b0307693a80280797
yangfeng7 <yangfeng7@wps.cn>
2024-10-04 18:21:21 +0800

    【项目：GoChatOnline】【ones:22】【改动：新增一个Message结构体统一消息格式，将聊天消息抽象为一个对象，包含了 Sender（发送者）、Content（消息内容）、和 Type（消息类型）】

---
+++ chatonlinev2/server/message.go
@@ -0,0 +1,8 @@
+package main
+
+// Message结构体，用于封装聊天消息
+type Message struct {
+	Sender  string `json:"sender"`  // 消息发送者
+	Content string `json:"content"` // 消息内容
+	Type    string `json:"type"`    // 消息类型，如"broadcast"（群聊）或"private"（私聊）
+}
