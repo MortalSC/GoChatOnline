d5529967b874412914456c9c03f2c9dda0c34f65
yangfeng7 <yangfeng7@wps.cn>
2024-10-04 21:41:40 +0800

    【项目：GoChatOnline】【ones:27】【改动：重构客户端client.go实现逻辑，引入Message的使用，加入json格式化数据传输】

--- chatonlinev2/client/client.go
+++ chatonlinev2/client/client.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 type Client struct {
@@ -13,18 +15,11 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
-	flag       int // 当前客户端选择的模式
+	mode       int // 当前客户端选择的模式：1.群聊；2.私聊；3.修改用户名
 }
 
 // 创建一个客户端
 func NewClient(serverIp string, serverPort int) *Client {
-	// 创建客户端对象
-	clinet := &Client{
-		ServerIp:   serverIp,
-		ServerPort: serverPort,
-		flag:       99,
-	}
-
 	// 连接 Server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
 	if err != nil {
@@ -32,10 +27,214 @@ func NewClient(serverIp string, serverPort int) *Client {
 		return nil
 	}
 
-	clinet.conn = conn
+	// 创建客户端对象
+	clinet := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+		conn:       conn,
+		mode:       0,
+	}
 
 	// 返回对象
 	return clinet
+}
+
+// 展示客户端功能
+func (client *Client) menu() bool {
+	var mode int
+	fmt.Println("1. Group chat mode")
+	fmt.Println("2. Private chat mode")
+	fmt.Println("3. Update user name")
+	fmt.Println("0. Quit")
+
+	fmt.Scanln(&mode)
+	if mode >= 0 && mode <= 3 {
+		client.mode = mode
+		return true
+	} else {
+		fmt.Println("Invalid option, please choose again.")
+		return false
+	}
+}
+
+// 发送消息到服务器
+func (client *Client) SendMessage(content string) {
+	msg := Message{
+		Sender:  client.Name, // 消息发送者
+		Content: content,     // 消息内容
+		Type:    "broadcast", // 默认消息为群聊
+	}
+
+	// 消息编码为json格式
+	data, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println("json.Marshal err : ", err)
+		return
+	}
+
+	client.conn.Write(data) // 通过连接发送消息
+}
+
+// 群聊业务
+func (client *Client) GroupChat() {
+
+	fmt.Println("Enter your message (type 'exit' to quit):")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		content, _ := reader.ReadString('\n')
+		content = strings.TrimSpace(content)
+
+		if content == "exit" {
+			break
+		}
+
+		// 创建群聊消息
+		msg := Message{
+			Sender:  client.Name,
+			Content: content,
+			Type:    "broadcast",
+		}
+
+		data, err := json.Marshal(msg)
+		if err != nil {
+			fmt.Println("json.Marshal err : ", err)
+			return
+		}
+
+		// 发送消息到服务器
+		client.conn.Write(data)
+	}
+}
+
+// 私聊业务
+func (client *Client) PrivateChat() {
+	fmt.Println("Enter the username you want to chat with:")
+	reader := bufio.NewReader(os.Stdin)
+	targetUser, _ := reader.ReadString('\n')
+	targetUser = strings.TrimSpace(targetUser)
+
+	fmt.Printf("You are now chatting privately with %s (type 'exit' to quit):\n", targetUser)
+
+	for {
+		content, _ := reader.ReadString('\n')
+		content = strings.TrimSpace(content)
+
+		if content == "exit" {
+			break
+		}
+
+		// 创建一个私聊消息
+		msg := Message{
+			Sender:  client.Name,
+			Content: content,
+			Type:    "private",  // 消息类型为私聊
+			Target:  targetUser, // 私聊的目标用户
+		}
+
+		data, err := json.Marshal(msg)
+		if err != nil {
+			fmt.Println("json.Marshal err : ", err)
+			return
+		}
+
+		client.conn.Write(data) // 发送消息到服务器
+	}
+}
+
+// 更新用户名
+func (client *Client) UpdateUsername() {
+	fmt.Println("Enter your new username:")
+	reader := bufio.NewReader(os.Stdin)
+	newName, _ := reader.ReadString('\n')
+	newName = strings.TrimSpace(newName)
+
+	if newName == "" {
+		fmt.Println("Username cannot be empty.")
+		return
+	}
+
+	client.Name = newName
+
+	msg := Message{
+		Sender:  "System",
+		Content: client.Name,
+		Type:    "rename",
+	}
+
+	data, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println("json.Marshal err:", err)
+		return
+	}
+
+	_, err = client.conn.Write(data) // 发送更新用户名消息
+	if err != nil {
+		fmt.Println("Failed to send message:", err)
+	}
+}
+
+// 处理Server端回应的操作消息
+// 比如修改用户成功后，提示的：修改成功等内容
+func (client *Client) DealResponse() {
+	// 一旦client有数据，就拷贝到标准输出，永久阻塞
+	//io.Copy(os.Stdout, client.conn)
+
+	reader := bufio.NewReader(client.conn)
+	for {
+		// 读取从服务器接收到的消息，按行读取
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Read error : ", err)
+			return
+		}
+
+		// 去除读取到的换行符
+		msg = strings.TrimSpace(msg)
+
+		// 解析 json
+		var message Message
+		if err := json.Unmarshal([]byte(msg), &message); err != nil {
+			fmt.Println("Unmarshal error : ", err)
+			continue
+		}
+
+		// 根据消息的类型和内容显示
+		switch message.Type {
+		case "broadcast":
+			fmt.Printf("[Broadcast] %s: %s\n", message.Sender, message.Content)
+		case "private":
+			fmt.Printf("[Private] %s: %s\n", message.Sender, message.Content)
+		case "info":
+			fmt.Printf("[Info] %s\n", message.Content)
+		default:
+			fmt.Printf("[Unknown] %s: %s\n", message.Sender, message.Content)
+		}
+	}
+}
+
+// 业务处理
+func (client *Client) Run() {
+	for {
+		if client.menu() {
+
+			// 根据不同的模式，进行不同的业务
+			switch client.mode {
+			case 1: // 群聊
+				//fmt.Println("select : Group chat mode...")
+				client.GroupChat()
+			case 2: // 私聊
+				//fmt.Println("select : Private chat mode...")
+				client.PrivateChat()
+			case 3: // 修改用户名
+				//fmt.Println("select : Update user name...")
+				client.UpdateUsername()
+			case 0:
+				fmt.Println("Exiting...")
+				return // 退出
+			}
+		}
+	}
 }
 
 var serverIp string
@@ -46,154 +245,20 @@ func init() {
 	flag.IntVar(&serverPort, "port", 8888, "set server port (defualt 8888)")
 }
 
-// 展示客户端功能
-func (client *Client) menu() bool {
-	var flag int
-	fmt.Println("1. Group chat mode")
-	fmt.Println("2. Private chat mode")
-	fmt.Println("3. Update user name")
-	fmt.Println("0. Quit")
-
-	fmt.Scanln(&flag)
-	if flag >= 0 && flag <= 3 {
-		client.flag = flag
-		return true
-	} else {
-		fmt.Println(">>>>>> Invalid input <<<<<<")
-		return false
-	}
-}
-
-// 群聊业务
-func (client *Client) PublicChat() {
-	// 提示用户输入消息
-	var chatMsg string
-
-	fmt.Println(">>>>> please input msg(to public)[exit means overchat] : ")
-	fmt.Scanln(&chatMsg)
-
-	for chatMsg != "exit" {
-		// 发送给服务器
-		// 消息非空就发送
-		if len(chatMsg) != 0 {
-			sendMsg := chatMsg + "\n"
-			_, err := client.conn.Write([]byte(sendMsg))
-			if err != nil {
-				fmt.Println("conn Write err : ", err)
-				break
-			}
-		}
-
-		chatMsg = ""
-		fmt.Println(">>>>> please input msg(to public)[exit means overchat] : ")
-		fmt.Scanln(&chatMsg)
-	}
-}
-
-// 查询在线用户
-func (client *Client) SelectUser() {
-	sendMsg := "who\n"
-	_, err := client.conn.Write([]byte(sendMsg))
-	if err != nil {
-		fmt.Println("conn.Write err : ", err)
-		return
-	}
-}
-
-// 私聊业务
-func (client *Client) PrivateChat() {
-	var remoteName string
-	var chatMsg string
-
-	client.SelectUser()
-	fmt.Println(">>>>> please input name to private chat, [exit meas overchat] : ")
-	fmt.Scanln(&remoteName)
-
-	for remoteName != "exit" {
-		fmt.Println(">>>>> please input msg(to private)[exit means overchat] : ")
-		fmt.Scanln(&chatMsg)
-
-		for chatMsg != "exit" {
-			// 发送给服务器
-			// 消息非空就发送
-			if len(chatMsg) != 0 {
-				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
-				_, err := client.conn.Write([]byte(sendMsg))
-				if err != nil {
-					fmt.Println("conn Write err : ", err)
-					break
-				}
-			}
-
-			chatMsg = ""
-			fmt.Println(">>>>> please input msg(to private)[exit means overchat] : ")
-			fmt.Scanln(&chatMsg)
-		}
-
-		client.SelectUser()
-		fmt.Println(">>>>> please input name to private chat, [exit meas overchat] : ")
-		fmt.Scanln(&remoteName)
-	}
-}
-
-// 更新用户名
-func (client *Client) UpdateName() bool {
-	fmt.Println(">>>>> please input new name :")
-	fmt.Scanln(&client.Name)
-
-	sendMsg := "rename|" + client.Name + "\n"
-	_, err := client.conn.Write([]byte(sendMsg))
-	if err != nil {
-		fmt.Println("conn.Write err : ", err)
-		return false
-	}
-
-	return true
-}
-
-// 处理Server端回应的操作消息
-// 比如修改用户成功后，提示的：修改成功等内容
-func (client *Client) DealResponse() {
-	// 一旦client有数据，就拷贝到标准输出，永久阻塞
-	io.Copy(os.Stdout, client.conn)
-}
-
-// 业务处理
-func (client *Client) Run() {
-	for client.flag != 0 {
-		for !client.menu() {
-
-		}
-
-		// 根据不同的模式，进行不同的业务
-		switch client.flag {
-		case 1: // 群聊
-			//fmt.Println("select : Group chat mode...")
-			client.PublicChat()
-		case 2: // 私聊
-			//fmt.Println("select : Private chat mode...")
-			client.PrivateChat()
-		case 3: // 修改用户名
-			//fmt.Println("select : Update user name...")
-			client.UpdateName()
-		}
-	}
-}
-
 func main() {
 	// 命令行解析
 	flag.Parse()
 
 	client := NewClient(serverIp, serverPort)
 	if client == nil {
-		fmt.Println(">>>>>>>>>>>>>>> connect Server failed...")
+		fmt.Println("Connection failed...")
 		return
 	}
 
 	// 处理Server回应的操作提示消息
 	go client.DealResponse()
 
-	fmt.Println(">>>>>>>>>>>>>>> connect Server successful...")
+	fmt.Println("Connection Server successful...")
 
 	// 启动客户端业务
 	client.Run()
